8c2c9b8043dd5db7138fee6be038548aa13b5156
BassXiaoSen <314121378@qq.com>
2018-05-19 00:59:55 +0800

    引入styled-component并使用

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
 class Home extends Component {
   render() {
-    return <div>Home</div>
+    return <Wrap>Home</Wrap>
   }
 }
 
 export default Home
+
+const Wrap = styled.div`
+  height: 100vh;
+  background: #00bcd4;
+`
