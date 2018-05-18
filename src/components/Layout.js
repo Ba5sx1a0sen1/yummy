bd6a259cbe8f0eb84c2799d408fd15f5e0e52ebf
BassXiaoSen <314121378@qq.com>
2018-05-19 01:35:35 +0800

    布局文件中添加路由规则

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import SignupContainer from '../containers/SignupContainer'
+import LoginContainer from '../containers/LoginContainer'
+import styled from 'styled-components'
 
-class Layout extends Component {
-  render() {
-    return <div>Layout</div>
-  }
-}
+const Layout = () => (
+  <Wrap>
+    <Header>页面标题</Header>
+    <Switch>
+      <Route path="/signup" component={SignupContainer} />
+      <Route path="/login" component={LoginContainer} />
+    </Switch>
+  </Wrap>
+)
 
 export default Layout
+
+const Wrap = styled.div``
+
+const Header = styled.div``
