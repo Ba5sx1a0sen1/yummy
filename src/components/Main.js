f219d86d0b9e242564e853d19826ca0eb6e8ef33
BassXiaoSen <314121378@qq.com>
2018-05-19 00:57:38 +0800

    添加home组件

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,14 @@
-import React from "react"
+import React, { Component } from 'react'
+import HomeContainer from '../containers/HomeContainer'
 
-class Main extends React.Component{
-    render(){
-        return <div>Main</div>
-    }
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <HomeContainer />
+      </div>
+    )
+  }
 }
 
 export default Main
---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Home from '../components/Home'
+
+const HomeContainer = props => <Home {...props} />
+//函数式组件，只做容器
+
+export default HomeContainer
