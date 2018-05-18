a22342d3145f9a72adccda03c0c38fbf2374a274
BassXiaoSen <314121378@qq.com>
2018-05-19 02:32:54 +0800

    引入好用的redux redux-thunk react-redux redux-logger并使用

--- src/containers/LayoutContainer.js
+++ src/containers/LayoutContainer.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { getTitle } from '../selectors'
+import { connect } from 'react-redux'
 
 const LayoutContainer = props => <Layout {...props} />
+const mapStateToProps = state => ({
+    title: getTitle(state)
+})
 
-export default LayoutContainer
+export default connect(mapStateToProps)(LayoutContainer)
