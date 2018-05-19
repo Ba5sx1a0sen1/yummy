1eb46597722c8c6c02825f6a7a38b1af829b3a90
BassXiaoSen <314121378@qq.com>
2018-05-19 15:32:59 +0800

    添加走马灯样式

--- src/components/Dishes.js
+++ src/components/Dishes.js
@@ -1,13 +1,62 @@
 import React, { Component } from 'react'
+import Slider from 'react-slick'
+import styled from 'styled-components'
 
 class Dishes extends Component {
     render() {
+        const settings = {
+            dots: true,
+            infinite: false,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false
+        }
         return (
-            <div>
-                Dishes
-        </div>
+            <Wrap>
+                <Slider {...settings}>
+                    {
+                        [
+                            <Card key="1">
+                                <DishesCard />
+                            </Card>,
+                            <Card key="2">
+                                <DishesCard />
+                            </Card>
+                        ]
+                    }
+                </Slider>
+            </Wrap>
         )
     }
 }
 
 export default Dishes
+
+const Wrap = styled.div`
+  height: 100%;
+  padding-bottom: 90px;
+  .slick-dots {
+    bottom: -40px;
+    li.slick-active button:before {
+      opacity: .9;
+      color: white;
+    }
+    li button:before {
+      opacity: .4;
+      color: white;
+    }
+  }
+`
+
+const Card = styled.div`
+  padding: 35px;
+  padding-top: 5px;
+  padding-bottom: 0;
+  height: 100%;
+`
+
+const DishesCard = styled.div`
+  height: 70vh;
+  background: white;
+`
